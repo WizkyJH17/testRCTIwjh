c1d3cedbc4268f92a7cd6cd023b51349b4bd4f5b
William Jh <developer17mail@gmail.com>
2024-07-04 15:19:23 +0700

    Initial Commit

---
+++ R+/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  R+
+//
+//  Created by William Johanssen Hutama on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
